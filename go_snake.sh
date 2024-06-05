f507bdc4240d678865d77d06c2b719f4fda145bf
Javier Moldon <jmoldon@users.noreply.github.com>
2024-06-05 12:09:12 +0200

    Update go_snake.sh
    add summary option for dag plots

--- go_snake.sh
+++ go_snake.sh
@@ -5,6 +5,16 @@ projectFolder="."
 envName="snakemake_env"
 snakemakeVersion="8.4.6"
 envFile="$projectFolder/environment.yaml"
+summary=false
+
+# Parse arguments
+for arg in "$@"
+do
+    if [ "$arg" == "--summary" ]; then
+        summary=true
+        break
+    fi
+done
 
 # Create and activate the Conda environment if it doesn't exist
 if ! conda env list | grep -q $envName; then
@@ -21,13 +31,22 @@ fi
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate $envName
 
-# Run Snakemake
-snakemake -s "$projectFolder/workflow/Snakefile" \
-          --configfile "$projectFolder/config/config.yaml" \
-          --cores 1 \
-          --latency-wait 60 \
-          "$@"
+# Produce supporting plots instead of running Snakemake if the summary option is selected
+if [ "$summary" == true ]; then
+    mkdir -p summary
+    snakemake --dag --forceall | dot -Tpng > summary/dag.png
+    snakemake --rulegraph --forceall | dot -Tpng > summary/rulegraph.png
+    snakemake --filegraph --forceall | dot -Tpng > summary/filegraph.png
+else
+    # Run Snakemake
+    snakemake -s "$projectFolder/workflow/Snakefile" \
+              --configfile "$projectFolder/config/config.yaml" \
+              --cores "$cores" \
+              --use-conda \
+              -p \
+              --latency-wait 60 \
+              "$@"
+fi
 
-# Deactivate the Conda environment
+# Deactivate the conda environment
 conda deactivate
-
